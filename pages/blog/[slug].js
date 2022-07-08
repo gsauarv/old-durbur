6400700b59c5e2018451285f3c924878a0087b8e
gsauarv <gsaurav2000@gmail.com>
2022-07-08 12:41:35 +0545

    a

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -5,57 +5,49 @@ const graphcms = new GraphQLClient(
   "https://api-ap-south-1.graphcms.com/v2/cl4rueg1b1kxe01zc1hute3rg/master"
 );
 
-function BlogDetail({ posts }) {
+function BlogDetail({ title, id }) {
   return (
     <>
-      {posts.map((post) => (
-        <div key={post.id}>
-          <PostComponent title={post.title} blogContent={post.content.html} />
-        </div>
-      ))}
+      <h2>{title}</h2>
+      <h2>{id}</h2>
     </>
   );
 }
 
 export default BlogDetail;
 
-export const getStaticProps = async ({ params }) => {
-  const slugId = params.slug;
-  const { posts } = await graphcms.request(gql`
-    {
-      posts(where: { slug:"${slugId}"  }) {
-        id
-        title
-        content{
-            html
-        }
-      }
-    }
-  `);
-  console.log(posts);
+export async function getStaticProps({ params }) {
+  const slug = params.slug;
+  const { post } = await graphcms.request(
+    gql`
+      query {
+        post(where: { slug: "${slug}" }) {
+          id
+          title
+        }} `
+  );
+
   return {
     props: {
-      posts: posts,
+      ...post,
     },
-    revalidate: 5,
   };
-};
+}
 
-export const getStaticPaths = async () => {
-  const { posts } = await graphcms.request(gql`
-    {
-      posts {
-        slug
+export async function getStaticPaths() {
+  const { posts } = await graphcms.request(
+    gql`
+      query {
+        posts {
+          slug
+        }
       }
-    }
-  `);
-  console.log(posts);
+    `
+  );
+
+  console.log(posts.map((post) => post.slug));
   return {
-    paths: posts.map((post) => ({
-      params: {
-        slug: post.slug,
-      },
-    })),
+    paths: posts.map((post) => "/blog/" + post.slug),
     fallback: true,
   };
-};
+}
